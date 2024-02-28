63ef312444236702b4d322d2a2dd1a6691dcd3a9
Matilda <mast1554@LAPTOP-RLVGD7S9.localdomain>
2024-02-28 15:44:12 +0100

    added blocks

--- sql/test.sql
+++ sql/test.sql
@@ -1,5 +1,7 @@
+-- @block
 CREATE DATABASE Store;
 
+-- @block
 CREATE TABLE Store.Products (
 	product_id int PRIMARY KEY AUTO_INCREMENT,
 	code varchar(4) UNIQUE,
@@ -7,31 +9,39 @@ CREATE TABLE Store.Products (
 	price float
 );
 
+-- @block
 CREATE TABLE Store.Suppliers (
 	supplier_id int PRIMARY KEY UNIQUE AUTO_INCREMENT,
 	supplier_name varchar(255) NOT NULL UNIQUE AUTO_INCREMENT,
 	supplier_phone varchar(10)
 );
 
+-- @block
 CREATE TABLE Store.ProductSuppliers (
 	product_id int NOT NULL,
 	supplier_id int NOT NULL
 );
 
+-- @block
 CREATE TABLE Store.Orders (
 	order_id int PRIMARY KEY AUTO_INCREMENT,
 	product_id int NOT NULL,
 	quantity int NOT NULL
 );
 
+-- @block
 INSERT INTO Store.Products (
 	code,
 	quantity,
 	price
 ) VALUES 
 ('abcd', 1, 20.65),
-('abcd', 2, 20.65),
-('abcd', 3, 20.65),
-('abcd', 4, 20.65),
-('abcd', 5, 20.65);
+('foo', 2, 30.65),
+('bar', 3, 40.65),
+('baz', 4, 50.65),
+('aaaa', 5, 60.65);
 
+-- @block
+SELECT * FROM Store.Products;
+SELECT * FROM Store.ProductSuppliers;
+SELECT * FROM Store.Orders;
